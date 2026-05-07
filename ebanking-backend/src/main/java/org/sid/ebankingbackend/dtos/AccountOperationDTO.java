ae61d89bdb46ed3d3a314204f66f6f36b6ba2caf
hichamouaouche <hichamouaouche@gmail.com>
2026-05-07 12:20:00 +0100

    feat: AccountHistoryDTO, AccountOperationDTO et BankAccountMapper entite-DTO

---
+++ ebanking-backend/src/main/java/org/sid/ebankingbackend/dtos/AccountOperationDTO.java
@@ -0,0 +1,20 @@
+package org.sid.ebankingbackend.dtos;
+
+import lombok.Data;
+
+import org.sid.ebankingbackend.enums.OperationType;
+
+import java.util.Date;
+
+@Data
+public class AccountOperationDTO {
+
+    private Long id;
+    private Date operationDate;
+    private double amount;
+    // to string
+
+    private OperationType type;
+
+    private String description;
+}
