600d6cd464d83439bca6bd739878621c09fc2741
hichamouaouche <hichamouaouche@gmail.com>
2026-05-13 15:10:00 +0100

    feat: DTOs LoginRequest et RegisterRequest pour les endpoints auth

---
+++ ebanking-backend/src/main/java/org/sid/ebankingbackend/dtos/RegisterRequest.java
@@ -0,0 +1,11 @@
+package org.sid.ebankingbackend.dtos;
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
